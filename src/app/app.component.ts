0d5d4a08e589095d5200c6b456a8d0fde1fdce6a
chelaruc <chelaru_ciprian@yahoo.com>
2017-10-06 17:23:44 +0300

    small changes for responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,12 +19,16 @@ export class AppComponent implements OnInit {
     ngOnInit() {
         var navbar : HTMLElement = this.element.nativeElement.children[0].children[0];
         this._router = this.router.events.filter(event => event instanceof NavigationEnd).subscribe((event: NavigationEnd) => {
-            window.document.children[0].scrollTop = 0;
+            if (window.outerWidth > 991) {
+                window.document.children[0].scrollTop = 0;
+            }else{
+                window.document.activeElement.scrollTop = 0;
+            }
             this.navbar.sidebarClose();
         });
         this.renderer.listenGlobal('window', 'scroll', (event) => {
             const number = window.scrollY;
-            if (number > 150) {
+            if (number > 150 || window.pageYOffset > 150) {
                 // add logic
                 navbar.classList.remove('navbar-transparent');
             } else {
@@ -32,6 +36,19 @@ export class AppComponent implements OnInit {
                 navbar.classList.add('navbar-transparent');
             }
         });
+        var ua = window.navigator.userAgent;
+        var trident = ua.indexOf('Trident/');
+        if (trident > 0) {
+            // IE 11 => return version number
+            var rv = ua.indexOf('rv:');
+            var version = parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+        }
+        if (version) {
+            var body = document.getElementsByTagName('body')[0];
+            body.classList.add('ie-background');
+
+        }
+
     }
     removeFooter() {
         var titlee = this.location.prepareExternalUrl(this.location.path());
