4410c708302df9934490d4daf8f5af97febf5a6f
chelaruc <chelaru_ciprian@yahoo.com>
2017-10-06 13:00:37 +0300

    small changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Inject, Renderer, ElementRef } from '@angular/core';
+import { Component, OnInit, Inject, Renderer, ElementRef, ViewChild } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
 import 'rxjs/add/operator/filter';
 import { DOCUMENT } from '@angular/platform-browser';
 import { LocationStrategy, PlatformLocation, Location } from '@angular/common';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @Component({
     selector: 'app-root',
@@ -12,12 +13,14 @@ import { LocationStrategy, PlatformLocation, Location } from '@angular/common';
 })
 export class AppComponent implements OnInit {
     private _router: Subscription;
+    @ViewChild(NavbarComponent) navbar: NavbarComponent;
 
     constructor( private renderer : Renderer, private router: Router, @Inject(DOCUMENT,) private document: any, private element : ElementRef, public location: Location) {}
     ngOnInit() {
         var navbar : HTMLElement = this.element.nativeElement.children[0].children[0];
         this._router = this.router.events.filter(event => event instanceof NavigationEnd).subscribe((event: NavigationEnd) => {
             window.document.children[0].scrollTop = 0;
+            this.navbar.sidebarClose();
         });
         this.renderer.listenGlobal('window', 'scroll', (event) => {
             const number = window.scrollY;
