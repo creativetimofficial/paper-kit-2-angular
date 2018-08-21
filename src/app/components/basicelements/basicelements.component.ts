0b178d4bad9e88ffbbe8c7cc01ae05a5b20b2e7e
chelaruc <chelaru_ciprian@yahoo.com>
2018-08-21 12:10:58 +0300

    fixes

--- src/app/components/basicelements/basicelements.component.ts
+++ src/app/components/basicelements/basicelements.component.ts
@@ -9,7 +9,7 @@ export class BasicelementsComponent implements OnInit {
     simpleSlider = 40;
     doubleSlider = [20, 60];
     state_default: boolean = true;
-
+    focus: any;
     constructor() { }
 
     ngOnInit() {}
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class LandingComponent implements OnInit {
+  focus: any;
+  focus1: any;
 
   constructor() { }
 
