b7b5752cb74eb9919c87d0639e686b1a2e311a6a
Kamal Karwal <kamal@karwal.live>
2024-03-16 16:30:33 +1030

    removed components folder

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
 
 @Component({
     selector: 'app-landing',
     templateUrl: './landing.component.html',
-    styleUrls: ['./landing.component.scss']
+    styleUrls: ['./landing.component.scss'],
 })
 
 export class LandingComponent implements OnInit {
